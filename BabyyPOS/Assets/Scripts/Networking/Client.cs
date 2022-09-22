562807f256d49b415ec667e67a621ac3ea594e50
BabeyJ <jackbabey@gmail.com>
2022-09-22 12:24:18 +0100

    Created Client setup GUI

--- BabyyPOS/Assets/Scripts/Networking/Client.cs
+++ BabyyPOS/Assets/Scripts/Networking/Client.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5ba5a433bd00886d0b45e5f8e8e10ec421fe462be079e71fc7634beed68483e
-size 2195
+oid sha256:9d285460d31e69d168ef103f408966535ae4882a0f0dbca6884fa983d4d6bd67
+size 3072
